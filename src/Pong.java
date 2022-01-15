87a05ca56da4e78366bf517ae955f4979620beba
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2022-01-15 12:55:34 -0800

    applet

--- src/Pong.java
+++ src/Pong.java
@@ -1,21 +1,78 @@
-import javax.swing.JFrame;
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Pong {
+public class Pong extends Applet implements Runnable, KeyListener {
+	final int WIDTH = 700;
+	final int HEIGHT = 500;
+	Thread thread;
+	
+	public void init() {
 
-	public Pong(String title, Game game) {
+		this.resize(WIDTH, HEIGHT);
+		this.addKeyListener(this);
+		thread = new Thread(this);
+		thread.start();
 		
-	JFrame frame = new JFrame();
-	
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setResizable(false);
-	frame.add(game);
-	frame.pack();
-	frame.setLocationRelativeTo(null);
-	frame.setVisible(true);
-	
-	
-	game.start();
-	
-	
 	}
+
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+	}
+
+	public void update(Graphics g) {
+		paint(g);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (;;) {
+
+			repaint();
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
